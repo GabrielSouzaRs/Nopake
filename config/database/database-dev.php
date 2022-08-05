abed2541e53cace7fe10cb3a6f70cf91a9fa57e1
Paulo Leonardo <pauloleonardo.rio@gmail.com>
2022-08-05 13:51:35 -0300

    Primeira versão beta

---
+++ config/database/database-dev.php
@@ -0,0 +1,29 @@
+<?php
+$erp = [
+    'host'=>'localhost',
+    'name'=>'erp',
+    'user'=>'root',
+	'pass'=>'',
+	'sgbd'=>'mysql',
+	'pref'=>''
+];
+
+$teste = [
+    'host'=>'localhost',
+    'name'=>'teste',
+    'user'=>'root',
+	'pass'=>'',
+	'sgbd'=>'mysql',
+	'pref'=>''
+];
+
+$ecommerce = [
+    'host'=>'172.106.0.110',
+    'name'=>'russel_ecommerce',
+    'user'=>'russel_ecommerce',
+	'pass'=>'Br@24015543005',
+	'sgbd'=>'mysql',
+	'pref'=>''
+];
+
+return $erp;
